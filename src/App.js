32264fa2aeb11a3e6f6f891f3bc2ecb608459ae2
gogo <himanshu.c.k.h@gmail.com>
2021-09-09 09:42:20 +0530

    removed security risks

--- src/App.js
+++ src/App.js
@@ -2,9 +2,7 @@ import React, { Component } from 'react';
 import Header from './components/Header';
 import About from './components/About';
 import Resume from './components/Resume';
-import Portfolio from './components/Portfolio';
 import Testimonials from  './components/Testimonials';
-import ContactUs from './components/ContactUs';
 import Footer from './components/Footer';
 import resumeData from './resumeData';
 class App extends Component {
--- src/components/ContactUs.js
+++ src/components/ContactUs.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 export default class ContactUs extends Component {
   render() {
-    let resumeData = this.props.resumeData;
     return (
       <section id="contact">
           <div className="row section-head">
